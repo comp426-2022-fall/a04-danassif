9daea513f0ec9841bdd4824213f3f1a4654c23b6
David Nassif <danassif@live.unc.edu>
2022-12-01 00:49:37 -0500

    Trying to fix module not found error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 import minimist from "minimist";
 import express from 'express';
-import {roll} from "../lib/roll.js";
+import {roll} from "./lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
 
